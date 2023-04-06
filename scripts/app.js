2903202d4d3799d401288b112a77c1ef63014830
tuba qureshi <tubaqureshi113@gmail.com>
2023-04-07 00:08:05 +0500

    zakatpercentage

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
+// Day 1 - 6 APRIL 2023
+
+
 // alert('bano qabil')
 // document.write("<h1>hello</h1>") 
 // console.log("123")
 
-var name = 'usama'
-var age = 24
-console.log(name)
+// var name = 'usama'
+// var age = 24
+// console.log(name)
 
 // alert('hello '+ name + ' kesse ho' + age)
-document.write("<h1>hello "+ age +"</h1>")
+// document.write("<h1>hello "+ age +"</h1>")
 // console.log(typeof age)
 
 // console.log(2+3)
@@ -17,7 +20,24 @@ document.write("<h1>hello "+ age +"</h1>")
 // console.log(2**3)
 
 // console.log(1/(3+(6*7)/(6+4)))
-var age =+prompt("Your Name Please")
-var age1 =+prompt("Your Father Please")
+// var age =+prompt("Your Name Please")
+// var age1 =+prompt("Your Father Please")
+
 // console.log(+Number(age)+Number(age1))
-console.log(age + age1)
+// console.log(age + age1)
+
+
+// Day 2  6 APRIL 2023
+
+
+var zakatpercentage = 0.025;
+
+var userInput = prompt("user value");
+
+userInput = parseInt("user value");
+
+var result = zakatpercentage * userInput;
+
+alert("Your zakat value is " +0.025);
+
+
