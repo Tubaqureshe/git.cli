2cae5e64b34e900047656358f10d44c914b3b620
tuba qureshi <MY_NAME@example.com>
2023-04-06 14:53:27 +0500

    FEAT:Initialize Projcet

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+// alert('bano qabil')
+// document.write("<h1>hello</h1>") 
+// console.log("123")
+
+var name = 'usama'
+var age = 24
+console.log(name)
+
+// alert('hello '+ name + ' kesse ho' + age)
+document.write("<h1>hello "+ age +"</h1>")
+// console.log(typeof age)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3)
+
+// console.log(1/(3+(6*7)/(6+4)))
+var age =+prompt("Your Name Please")
+var age1 =+prompt("Your Father Please")
+// console.log(+Number(age)+Number(age1))
+console.log(age + age1)
